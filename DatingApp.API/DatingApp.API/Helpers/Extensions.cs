3f16e13b45dcb019a556abb0adf9d4e2a5a9430f
Ntsieni Tshikhakhisa <tshikhakhisan@gmail.com>
2020-08-12 03:48:44 +0200

    Added error handling for angular and API

---
+++ DatingApp.API/DatingApp.API/Helpers/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Helpers
+{
+    //extensions do not need to create a new instance hence we add static key 
+    public static class Extensions
+    {
+        /*This class will be responsible for writing general extentions for all other extensions 
+         * we need
+         */
+
+        //AddApplicationError overrides response hence we add 'this' before HttpResponse to override response
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            // 
+            response.Headers.Add("Application-Error", message);
+
+            // CORS Headers for angular to get proper access control header
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+
+            // To allow cross origin between application eg angular from any origin
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
